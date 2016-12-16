e4de089fe2c3f2136c1c488d86b7c9ea73c75d52
Justin G. & swwhitaker <pair+justgrasse.swwhitaker@devbootcamp.com>
2016-12-16 14:47:09 -0800

    wrote empty? tests and method for Deck class

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -4,6 +4,7 @@ describe Deck do
 
   let(:deck) { Deck.new([['Q1','A1'],['Q2','A2']]) }
   let(:empty_deck) { Deck.new([]) }
+  let(:card_1) { Card.new(['Q1','A1']) }
 
   it 'has cards' do
     expect(deck.cards).to all be_instance_of Card
@@ -15,6 +16,16 @@ describe Deck do
     end
     it "checks if the deck is not empty" do
       expect(deck.empty?).to be false
+    end
+  end
+
+  describe "#get_card" do
+    it "gets the next card in the deck" do
+      expect(deck.get_card).to eq card_1
+    end
+    it "removes card from the deck" do
+      deck.get_card
+      expect(deck.cards.length).to eq 1
     end
   end
 
