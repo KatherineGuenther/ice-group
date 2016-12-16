ae69ab68f309767cf4851aaf2a1c686b20d89065
Justin G. & swwhitaker <pair+justgrasse.swwhitaker@devbootcamp.com>
2016-12-16 14:29:14 -0800

    complete card class with tests; removes uneccessary file requirements

--- card.rb
+++ card.rb
@@ -1,2 +1,9 @@
 class Card
+  attr_reader :question, :answer
+
+  def initialize(card_array)
+     @question = card_array[0]
+     @answer =  card_array[1]
+  end
+
 end
--- deck.rb
+++ deck.rb
@@ -1,6 +1,7 @@
-require_relative 'nighthawk_flashcard_data'
-require_relative 'otter_flashcard_data'
-require_relative 'raccoon_flashcard_data'
+require_relative 'card'
 
 class Deck
+
+  def initialize()
+  end
 end
