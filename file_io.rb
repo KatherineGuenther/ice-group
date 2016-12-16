7eb9aac3dd4b0bc254bf0a3d1e7252581207a1d5
Justin G. & swwhitaker <pair+justgrasse.swwhitaker@devbootcamp.com>
2016-12-16 15:40:02 -0800

    add rspec test for FileLiterator.read_text_file method

--- file_io.rb
+++ file_io.rb
@@ -4,7 +4,7 @@ module FileLiterator
   def self.read_text_file(filename)
     file_object = File.new(filename)
     lines = file_object.readlines
-    lines = lines.delete_if {|index| index == "\n"}
+    clean_lines = lines.delete_if {|index| index == "\n"}
     result = []
     i = 0
     while i < lines.length
@@ -15,4 +15,4 @@ module FileLiterator
   end
 end
 
-FileLiterator.read_text_file('otter_flashcard_data.txt')
+p FileLiterator.read_text_file('otter_flashcard_data.txt')
