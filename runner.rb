7bda97c04f7ffdbc57be15bedbd46c3a88c7f2f8
Jake Kelly & Justin G <pair+jakemkelly.justgrasse@devbootcamp.com>
2016-12-16 12:12:22 -0800

    Adds runner/controller program

--- runner.rb
+++ runner.rb
@@ -1,6 +1,23 @@
 # STEP 1
 # User enters a file name to begin the game
+filename = ARGV[0]
 
+cards = read_text_file(filename)
+deck = Deck.new(cards)
+view = View.new
+
+until deck.empty?
+  current_card = deck.get_card
+  puts view.question(current_card)
+  guess = view.get_guess
+    if guess == current_card.answer
+      puts view.correct
+      game.increase_score
+    else
+      puts view.wrong
+    end
+    puts view.outcome(game.score)
+end
 # STEP 2
   # Until the deck is empty, game presents front of card
     # User enters guess
