dfd70bb07ae9e0d2ad903c78e23fea4009f7c2b8
Jake Kelly & Katherine Guenther <pair+jakemkelly.katherineguenther@devbootcamp.com>
2016-12-16 16:56:33 -0800

    Refactor FileLiterator

--- file_io.rb
+++ file_io.rb
@@ -3,14 +3,22 @@ require 'io/console'
 module FileLiterator
   def self.read_text_file(filename)
     file_object = File.new(filename)
-    lines = file_object.readlines
-    lines.delete_if {|index| index == "\n"}
-    result = []
-    i = 0
-    while i < lines.length
-      result << [lines[i].chomp, lines[i+1].chomp]
-      i += 2
+
+    # Get lines without blank lines
+    lines = get_lines(file_object)
+
+    # Combine each odd and even line into an array of two elements
+    lines.each_with_index do |line, idx|
+      lines[idx] = [line.chomp, lines[idx +1 ].chomp] if idx.even?
     end
-   result
+
+    # Discard the even-numbered lines
+    lines.select.each_with_index { |line, idx| idx.even? }
   end
+
+  # OUTPUT: ARRAY of lines without blanks
+  def self.get_lines(file_object)
+    file_object.readlines.delete_if {|line| line == "\n"}
+  end
+
 end
